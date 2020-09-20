4e33313720ee8f1808f8685cfb83c3d5438ff81e
baronnavy <69622551+baronnavy@users.noreply.github.com>
2020-09-20 11:00:53 +0900

    Update settings.py

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'azuresite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'sql_server.pyodbc',
         'NAME': 'djangopractice',
         'USER': 'uehara@Nwgh2018',
